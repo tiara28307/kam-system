bf4900ba3d8169055d179a259fdbb52b0592849d
tiara28307 <tiara.carroll@ttu.edu>
2022-02-13 23:32:05 -0600

    graphql federation for microservice architecture

---
+++ services/kyc-screening/service.js
@@ -0,0 +1,56 @@
+const { ApolloServer, gql } = require("apollo-server");
+const { buildSubgraphSchema } = require("@apollo/federation");
+
+const typeDefs = gql`
+  extend type Query {
+    me: User
+  }
+
+  type User @key(fields: "id") {
+    id: ID!
+    name: String
+    username: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    me() {
+      return users[0];
+    }
+  },
+  User: {
+    __resolveReference(object) {
+      return users.find(user => user.id === object.id);
+    }
+  }
+};
+
+const server = new ApolloServer({
+  schema: buildSubgraphSchema([
+    {
+      typeDefs,
+      resolvers
+    }
+  ])
+});
+
+server.listen({ port: 4001 }).then(({ url }) => {
+  console.log(`Server ready at ${url}`);
+});
+
+// Test
+const users = [
+  {
+    id: "1",
+    name: "Ada Lovelace",
+    birthDate: "1815-12-10",
+    username: "@ada"
+  },
+  {
+    id: "2",
+    name: "Alan Turing",
+    birthDate: "1912-06-23",
+    username: "@complete"
+  }
+];

d3a3ba7b1f291e2651bbef48ae37ac31c468e823
tiara28307 <tiara.carroll@ttu.edu>
2022-02-17 14:40:38 -0600

    client interface initial build for system

--- kam-client/src/App.js
+++ kam-client/src/App.js
@@ -1,34 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
-import { client } from './apollo-client/client';
-import { ApolloProvider } from '@apollo/client';
+/**
+=========================================================
+* Material Kit 2 React - v2.0.0
+=========================================================
 
-function App() {
+* Product Page: https://www.creative-tim.com/product/material-kit-react
+* Copyright 2021 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+import { useEffect } from "react";
+
+// react-router components
+import { Routes, Route, Navigate, useLocation } from "react-router-dom";
+
+// @mui material components
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+// Material Kit 2 React themes
+import theme from "assets/theme";
+import Presentation from "layouts/pages/presentation";
+
+// Material Kit 2 React routes
+import routes from "routes";
+
+export default function App() {
+  const { pathname } = useLocation();
+
+  // Setting page scroll to 0 when changing the route
+  useEffect(() => {
+    document.documentElement.scrollTop = 0;
+    document.scrollingElement.scrollTop = 0;
+  }, [pathname]);
+
+  const getRoutes = (allRoutes) =>
+    allRoutes.map((route) => {
+      if (route.collapse) {
+        return getRoutes(route.collapse);
+      }
+
+      if (route.route) {
+        return <Route exact path={route.route} element={route.component} key={route.key} />;
+      }
+
+      return null;
+    });
+
   return (
-    <ApolloProvider client={client}>
-        <div className='App'>
-            <p>
-                Test Page.
-            </p>
-        </div>
-    </ApolloProvider>
-    /* <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div> */
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Routes>
+        {getRoutes(routes)}
+        <Route path="/homepage" element={<Presentation />} />
+        <Route path="*" element={<Navigate to="/homepage" />} />
+      </Routes>
+    </ThemeProvider>
   );
 }
-
-export default App;

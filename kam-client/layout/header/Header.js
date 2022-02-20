7066173cfa172a5f90839fceaa6d2e4d94571ee6
tiara28307 <tiara.carroll@ttu.edu>
2022-02-19 19:45:56 -0600

    nextjs ui instead of basic react

---
+++ kam-client/layout/header/Header.js
@@ -0,0 +1,83 @@
+import React, { useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  Container,
+  NavLink,
+} from "reactstrap";
+import logo from "../../assets/images/logos/kam-logo.png";
+
+const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const router = useRouter();
+  const toggle = () => setIsOpen(!isOpen);
+  return (
+    <div className="topbar" id="top">
+      <div className="header6">
+        <Container className="po-relative">
+          <Navbar className="navbar-expand-lg h6-nav-bar">
+            <NavbarBrand href="/">
+              <Image src={logo} alt="wrapkit" width={70} height={75} />
+            </NavbarBrand>
+            <NavbarToggler onClick={toggle}>
+              <span className="ti-menu"></span>
+            </NavbarToggler>
+            <Collapse
+              isOpen={isOpen}
+              navbar
+              className="hover-dropdown ml-auto"
+              id="h6-info"
+            >
+              <Nav navbar className="ml-auto">
+                <NavItem>
+                  <Link href="/#about">
+                    <a
+                      className={
+                        router.pathname == "/"
+                          ? "text-white nav-link"
+                          : "nav-link"
+                      }
+                    >
+                      About
+                    </a>
+                  </Link>
+                </NavItem>
+                <NavItem>
+                  <Link href="/#contact">
+                    <a
+                      className={
+                        router.pathname == "/"
+                          ? "text-white nav-link"
+                          : "nav-link"
+                      }
+                    >
+                      Contact
+                    </a>
+                  </Link>
+                </NavItem>
+              </Nav>
+              <div className="act-buttons">
+                <NavLink
+                  href="https://wrappixel.com/templates/nextkit-nextjs-free-uikit"
+                  className="btn btn-light font-14"
+                  target="_blank"
+                >
+                  Login
+                </NavLink>
+              </div>
+            </Collapse>
+          </Navbar>
+        </Container>
+      </div>
+    </div>
+  );
+};
+
+export default Header;

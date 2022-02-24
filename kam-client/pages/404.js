e0df549a3b2c664553cbaf8400a3fe3c9a0aed1e
tiara28307 <tiara.carroll@ttu.edu>
2022-02-24 11:53:22 -0600

    update 404 error page

--- kam-client/pages/404.js
+++ kam-client/pages/404.js
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import Link from "next/link";
 import { Container, Row, Col, Button } from "reactstrap";
+import Image from "next/image";
+import errorImg from "../assets/images/errors/error404.png";
 
 const ErrorPage = () => {
   return (
@@ -15,14 +17,12 @@ const ErrorPage = () => {
       </Head>
       <Container>
         <Row className="justify-content-center">
-          <Col lg="8" md="6" className="align-self-center text-center">
-            <h1 className="title">404 - Error</h1>
-            <h4 className="subtitle font-light">
-              The page you are looking for is not available
-              <br /> please go back to homepage
-            </h4>
+          <Col lg="8" md="6" className="align-self-center text-center m-t-30">
+            <Image src={errorImg} alt="wrapkit" />
+            <h1 className="text-white">404 Error</h1>
+            <h4 className="text-white">Page could not be found please go back to homepage</h4>
             <Link href="/">
-              <Button className="btn btn-md m-t-30 btn-danger-gradiant font-14">
+              <Button className="btn btn-md m-t-30 m-b-30 btn-danger-gradiant font-14">
                 Back to Homepage
               </Button>
             </Link>

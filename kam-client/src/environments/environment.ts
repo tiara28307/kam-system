3a4969dc6acdc93092fcbc547b0b3eda6739676e
tiara28307 <tiara.carroll@ttu.edu>
2022-03-16 16:56:49 -0500

    update packages in backend and setup secret env configs for frontend angular app

--- kam-client/src/environments/environment.ts
+++ kam-client/src/environments/environment.ts
@@ -1,10 +1,3 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false,
-  privacyPolicyUrl: 'https://texastechuniversity-my.sharepoint.com/personal/tiara_carroll_ttu_edu/Documents/TiAra%20Carroll%20-%20CS%206001%20-%20Masters%20Project/privacy_policy.pdf',
-  termsAndConditionsUrl: 'https://texastechuniversity-my.sharepoint.com/personal/tiara_carroll_ttu_edu/Documents/TiAra%20Carroll%20-%20CS%206001%20-%20Masters%20Project/terms_and_conditions.pdf',
-  whitePaper: 'https://texastechuniversity-my.sharepoint.com/:w:/g/personal/tiara_carroll_ttu_edu/EUUsYaJ0T2BGkpsDC7ixZ0wBR3hcLJaKTWH1TPdQFfQ2OQ?e=UyvCio'
+   production: false,
 };

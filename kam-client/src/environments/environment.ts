b3136de640a7e3f4ad315db5d76c3b02df6ca80c
tiara28307 <tiara.carroll@ttu.edu>
2022-03-16 09:55:55 -0500

    remove env variables that are in repo

--- kam-client/src/environments/environment.ts
+++ kam-client/src/environments/environment.ts
@@ -6,7 +6,5 @@ export const environment = {
   production: false,
   privacyPolicyUrl: 'https://texastechuniversity-my.sharepoint.com/personal/tiara_carroll_ttu_edu/Documents/TiAra%20Carroll%20-%20CS%206001%20-%20Masters%20Project/privacy_policy.pdf',
   termsAndConditionsUrl: 'https://texastechuniversity-my.sharepoint.com/personal/tiara_carroll_ttu_edu/Documents/TiAra%20Carroll%20-%20CS%206001%20-%20Masters%20Project/terms_and_conditions.pdf',
-  whitePaper: 'https://texastechuniversity-my.sharepoint.com/:w:/g/personal/tiara_carroll_ttu_edu/EUUsYaJ0T2BGkpsDC7ixZ0wBR3hcLJaKTWH1TPdQFfQ2OQ?e=UyvCio',
-  cognitoUserPoolId: 'us-east-1_Ni8RE7ogX',
-  cognitoAppClientId: '62pogk1iesf3ssqbr315790ile'
+  whitePaper: 'https://texastechuniversity-my.sharepoint.com/:w:/g/personal/tiara_carroll_ttu_edu/EUUsYaJ0T2BGkpsDC7ixZ0wBR3hcLJaKTWH1TPdQFfQ2OQ?e=UyvCio'
 };

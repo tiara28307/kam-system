1a24a3f2f25e2286be94be0a9c73599d13bafd32
tiara28307 <tiara.carroll@ttu.edu>
2022-03-20 22:40:26 -0500

    company components

---
+++ kam-client/src/app/views/user/dashboards/company-case-management-dashboard/company-case-management-dashboard.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-company-case-management-dashboard',
+  templateUrl: './company-case-management-dashboard.component.html',
+  styles: [
+  ]
+})
+export class CompanyCaseManagementDashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
